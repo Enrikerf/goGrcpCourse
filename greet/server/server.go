ce55fe7fcfa441cdc39173595c0e47fc67bd759a
Enrique Arrabal <enrike9066@gmail.com>
2021-09-25 14:58:06 +0200

    first commit

---
+++ greet/server/server.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+
+	"proto"
+
+	"google.golang.org/grpc"
+)
+
+type server struct{}
+
+func main() {
+	fmt.Println("Hello world")
+
+	listener, error := net.Listen("tcp", "0.0.0.0:50051")
+	if error != nil {
+		log.Fatalf("failed to listen")
+	}
+	s := grpc.NewServer()
+	proto.RegisterGreetServiceServer(s, &server{})
+
+	if error := s.Serve(listener); error != nil {
+		log.Fatalf("falta")
+	}
+}
