71c81c62e2d4c8fb5d16fd1bf9d4714c57c833fe
Enrique Arrabal <Enrikerf@users.noreply.github.com>
2021-10-19 19:07:11 +0200

    Draft: implement script to generate certificates (#8)
    * implement script to generate certificates
    
    * generated but fails in handsake
    
    * fix ssl certs
    
    * parametric tls inability

--- greet/server/server.go
+++ greet/server/server.go
@@ -13,6 +13,7 @@ import (
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/credentials"
 	"google.golang.org/grpc/status"
 )
 
@@ -108,7 +109,20 @@ func main() {
 	if error != nil {
 		log.Fatalf("failed to listen")
 	}
-	s := grpc.NewServer()
+	tlsEnabled := true
+	var serverOptions grpc.ServerOption
+	if tlsEnabled {
+		certificate := "../../ssl/server.crt"
+		keyFile := "../../ssl/server.pem"
+		credentials, sslError := credentials.NewServerTLSFromFile(certificate, keyFile)
+		if sslError != nil {
+			log.Fatalf("error: %v", sslError)
+			return
+		}
+		serverOptions = grpc.Creds(credentials)
+	}
+
+	s := grpc.NewServer(serverOptions)
 	proto.RegisterGreetServiceServer(s, &server{})
 
 	if error := s.Serve(listener); error != nil {
